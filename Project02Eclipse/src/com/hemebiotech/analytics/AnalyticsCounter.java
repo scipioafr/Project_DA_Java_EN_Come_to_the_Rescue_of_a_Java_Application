3fb40cdcfc702c4ef194c08786c1d5a7dc8bc424
scipioafr <gmenoube@yahoo.fr>
2021-05-04 17:03:03 +0200

    m

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,72 +1,60 @@
 package com.hemebiotech.analytics;
 
+import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
+    private static int headacheCount = 0;    // initialize to 0
+    private static int rashCount = 0;        // initialize to 0
+    private static int pupilCount = 0;        // initialize to 0
 
-		//Première étape:on déclare une hashmap (de type (string,integer))
-		//On lit le fichier symptoms.txt(voir bufferedreader)
-		//On parcourt le fichier symptoms.txt en comptant les symptomes dans la map(voir la boucle while et les conditions)
-		//on range dans l'ordre alphabétique les symptomes utiliser la méthode Collections.sort
-		//On écrit dans le fichier result.out
+    public static void main(String args[]) throws Exception {
 
-
-        String []disease;
-        disease= new String[101];
-
-	    // first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+        //Première étape:on déclare une hashmap (de type (string,integer))
+        //On lit le fichier symptoms.txt(voir bufferedreader)
+        //On parcourt le fichier symptoms.txt en comptant les symptomes dans la map(voir la boucle while et les conditions)
+        //on range dans l'ordre alphabétique les symptomes utiliser la méthode Collections.sort
+        //On écrit dans le fichier result.out
 
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-        disease[i]=line;
-		while (line != null) {
-			i++;	// increment i
-			/*System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-			System.out.println("i:"+i);*/
+        Map<String, Integer> disease = new HashMap<String, Integer>();
 
-			line = reader.readLine();	// get another symptom
-            disease[i]=line;
-		}
-		i=0;
-		for(int j=0;j<=98;j++) {
-		    for(int k=(j+1);k<=99;k++){
-		        if(disease[j].equals(disease[k])){
-                    i++;
-                    disease[k] = ("rien");
-                }
+        // first get input
+        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+        String line = reader.readLine();
+
+        int i = 0;    // set i to 0
+        while (line != null) {
+            //i++;	// increment i
+
+
+            if (disease.containsKey(line)) {
+                i = disease.get(line) + 1;
+                disease.put(line, i);
+            } else {
+                disease.put(line, 1);
             }
-		    if(i==0){i++;}
-		    if(disease[j].equals("rien")){
+            line = reader.readLine();    // get another symptom
 
-            }else{System.out.println(disease[j]+":"+i);}
-		    i=0;
         }
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        List<String> symptoms= new ArrayList<>(disease.keySet());
+        Collections.sort(symptoms);
+
+
+        for (Map.Entry<String, Integer> maladie : disease.entrySet()) {
+            System.out.println(maladie.getKey() + ":" + maladie.getValue());
+        }
+
+
+        // next generate output
+        FileWriter writer = new FileWriter("result.out");
+
+        for (String maladie:symptoms) {
+            writer.write(maladie + ":" + disease.get(maladie) + "\n");
+        }
+        writer.close();
+    }
 }
